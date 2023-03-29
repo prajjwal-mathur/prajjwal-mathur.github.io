7fd6df94f3bf742d5459e8efa6bfd21731da8d9e
Prajjwal Mathur <64896645+SlowFlash22@users.noreply.github.com>
2023-03-30 00:33:51 +0530

    Resume link update

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CTA = () => {
   return (
     <div className="cta">
-        <a href="https://drive.google.com/file/d/1F6-slsr94raHtqRiwYxv0RODvDUoxPkg/view?usp=share_link" target="_blank" rel="noreferrer" className='btn'>View Resume</a>
+        <a href="https://drive.google.com/file/d/1Eu4SCfq5134TUEjEjVHXPmUJoyU6_2cr/view?usp=sharing" target="_blank" rel="noreferrer" className='btn'>View Resume</a>
         <a href="#contact" target="_blank" rel="noreferrer" className='btn btn-primary'>Contact me</a>
     </div>
   )
